daf8908c17cab14510f1256471d11dba26eb627f
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-09-10 18:31:50 +0100

    Update user experience when all task is deleted

--- src/App.js
+++ src/App.js
@@ -33,7 +33,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length>0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ("No Tasks To Manage")}
+      
     </div>
   );
 }
