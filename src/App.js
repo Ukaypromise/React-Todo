9f6a99526003b200d836b314851ddac35d42832c
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-09-10 20:31:02 +0100

    Add functionality to add task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import AddTask from './components/AddTask';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
@@ -27,6 +28,12 @@ const task = [
 function App() {
   const [tasks, setTasks] = useState(task);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -38,6 +45,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
